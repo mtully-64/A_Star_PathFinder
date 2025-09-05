a52417514bee41106567c072d82c129a1a6605cb
Mark Tully <marktully2012@gmail.com>
2025-09-05 13:45:34 +0100

    Implemented the grid and interaction of game

--- astar.py
+++ astar.py
@@ -2,6 +2,8 @@ import pygame
 import math
 from queue import PriorityQueue
 
+pygame.init()
+
 # Defining the width and height of my window for the game
 WIDTH = 800
 
@@ -16,8 +18,8 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
-WHITE = (0, 0, 255)
-BLACK = (255, 255, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 PURPLE = (128, 0, 128)
 ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
@@ -54,7 +56,10 @@ class Node:
         return self.colour == TURQUOISE
     
     def reset(self):
-        self.colour == WHITE
+        self.colour = WHITE
+
+    def make_start(self):
+        self.colour = ORANGE
     
     def make_closed(self):
         self.colour = RED
@@ -72,4 +77,107 @@ class Node:
         self.colour = PURPLE
 
     def draw(self, win):
-        pygame.draw.rect(win, self.colour, ())
+        pygame.draw.rect(win, self.colour, (self.x, self.y, self.width, self.width))
+
+    def update_neighbours(self, grid):
+        pass
+
+    def __lt__(self, other):
+        return False
+    
+# I need to make a heuristic function, this tells A* an estimate of the min cost from any vertex
+# This will be calculated using Manhattan Distance (straight line movements just like how the game will run)
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+# The game will need a platform/grid for visualisation and interaction
+# Here will will make the grid of the game's UI
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows
+
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i, j, gap, rows)
+            grid[i].append(node)
+
+
+    return grid
+
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+    for j in range(rows):
+        pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+
+
+def draw(win, grid, rows, width):
+    win.fill(WHITE)
+
+    for row in grid:
+        for node in row:
+            node.draw(win)
+    
+    draw_grid(win, rows, width)
+    pygame.display.update()
+
+def get_clicked_position(pos, rows, width):
+    gap = width // rows
+    y, x = pos
+
+    row = y // gap
+    col = x // gap
+
+    return row, col
+
+def main(win, width):
+    ROWS = 50
+    grid = make_grid(ROWS, width)
+
+    start = None
+    end = None
+    
+    run = True
+    started = False
+
+    while run:
+        draw(win, grid, ROWS, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if started: 
+                continue
+
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_position(pos, ROWS, width)
+                node = grid[row][col]
+                if not start and node != end:
+                    start = node
+                    start.make_start()
+
+                elif not end and node != start:
+                    end = node
+                    end.make_end()
+
+                elif node != end and node != start:
+                    node.make_barrier()
+
+            elif pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_position(pos, ROWS, width)
+                node = grid[row][col]
+                node.reset()
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
+
+    pygame.quit()
+
+main(WIN, WIDTH)
